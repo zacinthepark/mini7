8fdea55c6b756678ed981973cc0dcb5bcad80705
Jung <syl314@naver.com>
2024-06-12 09:31:44 +0900

    admin page

--- chat/admin.py
+++ chat/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from .models import History
 
 class HistoryAdmin(admin.ModelAdmin):
+    list_display = ['chat_time', 'query', 'answer']
+    list_display_links = ['query']
     ordering = ['chat_time']
     search_fields = ['query', 'answer']
     list_per_page = 10
-
+    
+    fieldsets = (
+        ('Q&A 내용', {'fields':('query', 'answer', 'chat_time')}),
+        ('Sim_score', {'fields':('sim1', 'sim2', 'sim3')}),
+    )
+    
 admin.site.register(History, HistoryAdmin)
